231cc168feb634e57370aabbd5581288e6ac97d6
Parkhomenko Egor <parkhomenko2049@gmail.com>
2023-11-16 00:22:12 +0600

    [Feat] Implement async logic in AudioPlayer

--- src/widgets/audioPlayer/ui/AudioPlayer.tsx
+++ src/widgets/audioPlayer/ui/AudioPlayer.tsx
@@ -1,11 +1,29 @@
 import React, { useState, type ElementRef, useRef, useEffect } from 'react';
+import { fetchChapter } from 'pages/main/api/fetchChapter';
 import backTo15Icon from 'shared/assets/back-to-15-icon.svg';
 import forwartTo15Icon from 'shared/assets/forward-to-15-icon.svg';
 import PauseIcon from 'shared/assets/pause-icon.svg';
 import PlayIcon from 'shared/assets/play-icon.svg';
 import styles from '../styles/AudioPlayer.module.css';
 
-const AudioPlayer: React.FC = props => {
+interface AudioPlayerProps {
+    chapter: string;
+}
+
+const AudioPlayer: React.FC<AudioPlayerProps> = props => {
+    const { chapter } = { ...props };
+    const [audio, setAudio] = useState<string | null>(null);
+    const [isLoaded, setLoaded] = useState<boolean>(false);
+    const [isLoading, setLoading] = useState<boolean>(false);
+    const [buttonText, setButtonText] = useState<string>(`Скачать главу ${+chapter + 1}`);
+
+    const fetchAudio = async () => {
+        if (!isLoading) {
+            const data = await fetchChapter(chapter);
+            return data.audio;
+        }
+    };
+
     // state
     const [isPlaying, setIsPlaying] = useState<Boolean>(false);
     const [duration, setDuration] = useState<number>(0);
@@ -16,11 +34,7 @@ const AudioPlayer: React.FC = props => {
     const progressBar = useRef<ElementRef<'input'>>(null); // reference our progress bar
     const animationRef = useRef<number>(); // reference the animation
 
-    useEffect(() => {
-        const seconds = Math.floor(audioPlayer?.current?.duration ?? 0);
-        setDuration(seconds);
-        progressBar.current!.max = seconds.toString();
-    }, [audioPlayer?.current?.onloadedmetadata, audioPlayer?.current?.readyState]);
+    useEffect(() => {}, [audioPlayer?.current?.onloadedmetadata, audioPlayer?.current?.readyState]);
 
     const calculateTime = (secs: number) => {
         const minutes = Math.floor(secs / 60);
@@ -31,6 +45,9 @@ const AudioPlayer: React.FC = props => {
     };
 
     async function togglePlayPause() {
+        const seconds = Math.floor(audioPlayer?.current?.duration ?? 0);
+        setDuration(seconds);
+        progressBar.current.max = seconds.toString();
         const prevValue = isPlaying;
         setIsPlaying(!prevValue);
         if (!prevValue) {
@@ -73,37 +90,58 @@ const AudioPlayer: React.FC = props => {
     };
 
     return (
-        <div className={'w-full bg-player-bg rounded-xl px-4'}>
-            <audio
-                ref={audioPlayer}
-                src='https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3'
-                preload='metadata'
-            ></audio>
-            <div className={'w-full flex flex-row justify-between items-center'}>
-                <button onClick={backThirty}>
-                    <img src={backTo15Icon} />
+        <div>
+            {!isLoaded && (
+                <button
+                    className={'bg-orange rounded-full py-4 mt-2 text-white w-full'}
+                    onClick={() => {
+                        setButtonText('Загрузка...  ');
+                        setLoading(true);
+                        void fetchAudio()
+                            .then(data => {
+                                if (data) {
+                                    setAudio(data);
+                                    setLoaded(true);
+                                }
+                            })
+                            .catch();
+                    }}
+                >
+                    {buttonText}
                 </button>
-                <button onClick={togglePlayPause} className={styles.playPause}>
-                    {isPlaying ? <img src={PauseIcon} /> : <img src={PlayIcon} />}
-                </button>
-                <button onClick={forwardThirty}>
-                    <img src={forwartTo15Icon} />
-                </button>
+            )}
+            {audio && (
+                <div className={'w-full bg-player-bg rounded-xl px-4'}>
+                    <audio ref={audioPlayer} preload='metadata'>
+                        <source src={`data:audio/mp3;base64,${audio}`} />
+                    </audio>
+                    <div className={'w-full flex flex-row justify-between items-center'}>
+                        <button onClick={backThirty}>
+                            <img src={backTo15Icon} />
+                        </button>
+                        <button onClick={togglePlayPause} className={styles.playPause}>
+                            {isPlaying ? <img src={PauseIcon} /> : <img src={PlayIcon} />}
+                        </button>
+                        <button onClick={forwardThirty}>
+                            <img src={forwartTo15Icon} />
+                        </button>
 
-                {/* current time */}
-                <div className={'text-orange font-Ubuntu px-2'}>{calculateTime(currentTime)}</div>
+                        {/* current time */}
+                        <div className={'text-orange font-Ubuntu px-2'}>{calculateTime(currentTime)}</div>
 
-                {/* progress bar */}
-                <div>
-                    <input
-                        type='range'
-                        className={styles.progressBar}
-                        defaultValue='0'
-                        ref={progressBar}
-                        onChange={changeRange}
-                    />
+                        {/* progress bar */}
+                        <div>
+                            <input
+                                type='range'
+                                className={styles.progressBar}
+                                defaultValue='0'
+                                ref={progressBar}
+                                onChange={changeRange}
+                            />
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
